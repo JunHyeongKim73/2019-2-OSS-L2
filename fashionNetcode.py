d5bb890b6ea21b9e9b69967ac31519e9475429e2
KimJunHyeong, KJH <rla5764v@gmail.com>
2019-12-02 23:34:57 +0900

    code addition_2

--- fashionNetcode.py
+++ fashionNetcode.py
@@ -32,3 +32,34 @@
     "\n",
     "import tensorflow.keras\n",
     "from PIL import Image\n",
+	"import glob\n",
+    "\n",
+    "from keras.preprocessing.image import ImageDataGenerator\n",
+    "from keras.models import Sequential, load_model\n",
+    "from keras.layers import Convolution2D, MaxPooling2D, ZeroPadding2D ,Conv2D\n",
+    "from keras.layers import Activation, Dropout, Flatten, Dense\n",
+    "from keras.callbacks import TensorBoard, ModelCheckpoint\n",
+    "from keras.layers import Input\n",
+    "from keras.applications import VGG16\n",
+    "from keras import backend\n",
+    "from keras import optimizers\n",
+    "from tensorflow.keras import applications\n",
+    "from keras.utils.conv_utils import convert_kernel\n",
+    "\n",
+    "\n",
+    "%matplotlib inline"
+   ]
+  },
+  {
+   "cell_type": "code",
+   "execution_count": null,
+   "metadata": {},
+   "outputs": [],
+   "source": [
+    "import tensorflow as tf\n",
+    "config = tf.ConfigProto()\n",
+    "\n",
+    "config.gpu_options.allow_growth = True\n",
+    "\n",
+    "sess = tf.Session(config=config)"
+   ]	
